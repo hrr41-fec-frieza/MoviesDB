a988d212574dd241d59f685a5703f09babc135f0
hay-0le <haley.olsen1@gmail.com>
2019-10-25 13:08:50 -0700

    Updating master to complete merge.

--- client/src/components/Movies.jsx
+++ client/src/components/Movies.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
-class Movies extends React.Component {
-  constructor(props) {
-    super(props)
+const MoviesDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 246px;
+  height: 238px;
+`
+const Movie = styled.div`
+  width: 30%;
+  margin: auto;
 
-    this.state = {
+`
 
-    }
-  }
+function Movies (props) {
+  var movies = props.movies;
+  var click = props.click;
+
+  return (
+    <MoviesDiv>
+      {[...movies].map((movie, i) =>
+
+        <Movie key={i} onClick={click}>
+          <img id={movie._id} src={(movie.pictureURL)} height="113" width="76" />
+        </Movie>
+      )}
+    </MoviesDiv>
+  )
 }
+
+export default Movies;
