d0ab8b3e2762d81f8828802f228feb5d59141095
hay-0le <haley.olsen1@gmail.com>
2020-01-26 21:28:53 -0800

    Moved styled components out of files, and into separate folder

--- client/src/components/Current.jsx
+++ client/src/components/Current.jsx
@@ -1,175 +1,54 @@
 import React from 'react';
-import styled from 'styled-components';
-// const {styled} = window;
+
 import StarRating from './StarRating.jsx';
+import {
+  CurrentMovie,
+  Button,
+  MovieInfo,
+  CurrentDiv,
+  Title,
+  Year,
+  TitleYear,
+  Rating,
+  Genre,
+  RatingGenre,
+  Description,
+  Directors,
+  Stars
+} from './componentStyling/CurrentStyling.js';
 
-
-const CurrentMovie =styled.div`
-  width: 127.986px;
-  height: 190px;
-  margin-left: 10px;
-  display:flex;
-  flex-direction: column;
-  box-shadow: 3px 3px 5px #B0B0B0;
-  cursor: pointer;
-`
-
-const Button = styled.button`
-  margin-left: 5.2;
-  margin-right: 5.2;
-  margin-top: 7px;
-  height: 17.778;
-  width: 106.424;
-  cursor: pointer;
-`
-
-const MovieInfo = styled.div`
-  width: 215px;
-  height: 265px;
-  margin: 5px;
-  padding: 5px;
-  display: block;
-
-`
-
-const CurrentDiv = styled.div`
-  height: 265px;
-  width: 360px;
-  margin: 5px;
-  margin-top: 0;
-  display:flex;
-`
-const Title = styled.span`
-  width: 126px;
-  height: 15px;
-  margin: 1px 5px;
-  font-size: 13px;
-  float: left;
-  line-height: 0.9;
-  color: #136CB2;
-  font-weight: bolder;
-  font-family: Verdana, Arial, sans-serif;
-  cursor: pointer;
-
-`
-const Year = styled.span`
-  width:46px;
-  height: 15px;
-  margin: 1px 5px;
-  font-size: 13px;
-  color: #999;
-
-  font-family: Verdana, Arial, sans-serif;
-`
-const TitleYear = styled.div`
-  width: 100%;
-  height: 24px;
-  margin-bottom: 5px;
-  position:relative;
-`
-
-const Rating = styled.span`
-  color: #999;
-  width: 44px;
-  color: #999;
-  border: 1px solid #999;
-  border-radius: 3px;
-  font-family: Verdana, Arial, sans-serif;
-  font-weight: bold;
-  font-size: 12px;
-  margin: 3px 3px;
-  padding: 0;
-`
-const Genre = styled.span`
-  font-size: 10px;
-  color: #999;
-  font-family: Verdana, Arial, sans-serif;
-  margin: 5px;
-
-`
-
-const RatingGenre = styled.div`
-  flex-basis: content;
-  margin: 3px;
-  position:relative;
-`
-
-const Description = styled.div`
-  font-family: Verdana, Arial, sans-serif;
-  color: #333;
-  font-size: 11px;
-  height: 120px;
-  width: 215px;
-  overflow: hidden;
-
-
-`
-
-const Directors = styled.div`
-  font-family: Verdana, Arial, sans-serif;
-  color: #333;
-  font-size: 11px;
-  margin:0px;
-  padding:0px;
-  position:relative;
-`
-
-const Stars = styled.div`
-  font-family: Verdana, Arial, sans-serif;
-  color: #333;
-  font-size: 11px;
-  margin:0px;
-  padding:0px;
-  position:relative;
-`
-
-function Current (props) {
-  var id = props.movie._id;
-  var picture = props.movie.pictureURL;
-  var title = props.movie.title.slice(0, 25);
-  var year = props.movie.year.toString().slice(0,4);
-  year = parseInt(year);
-  var description = props.movie.description;
-  var rating = props.movie.rating;
-  var splitGenre = props.movie.genre.split(',');
-  var genre = splitGenre.join('|');
-  var starRating = props.movie.starRating;
-  var director = props.movie.director;
-  var movieStars = props.movie.stars;
-
-  var click = props.click;
-
-  var clickStar = props.starClick;
-  var hover = props.hover;
-
+function Current ({ _id, movie: { pictureURL, title, year, description, rating, genre, starRating, director, stars }, click, starClick, hover }) {
 
   return (
     <CurrentDiv>
+
       <CurrentMovie id='current'>
-        <img src={picture} height="100%" width="100%" />
+        <img src={pictureURL} height="100%" width="100%" />
         <Button className='addToBtn'>Add to Watchlist</Button>
         <Button className='nextBtn' onClick={click}> Next >> </Button>
       </CurrentMovie>
+
       <MovieInfo>
         <TitleYear>
-          <Title>{title} </Title>
-          <Year>({year})</Year>
+          <Title>{title.slice(0, 25)} </Title>
+          <Year>({parseInt(year.toString().slice(0, 4))})</Year>
         </TitleYear>
+
         <RatingGenre>
           <Rating>{rating}</Rating>
-          <Genre>{genre}</Genre>
+          <Genre>{genre.split(',').join('|')}</Genre>
         </RatingGenre>
-        <StarRating rating={starRating} movieId={id} clickStar={clickStar} />
+
+        <StarRating rating={starRating} movieId={_id} clickStar={starClick} />
 
         <Description>{description}</Description>
         <Directors><b>Directors: </b>{director}</Directors>
-        <Stars><b>Stars: </b>{movieStars}</Stars>
+        <Stars><b>Stars: </b>{stars}</Stars>
 
       </MovieInfo>
-    </CurrentDiv>
 
+    </CurrentDiv>
   )
 }
-
 
 export default Current;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,86 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
-// const {styled} = window;
 import axios from 'axios';
 
 import { ThemeProvider } from 'styled-components';
 import { CSSTransition } from 'react-transition-group';
 import { TransitionGroup } from 'react-transition-group';
 
+import {
+  MainContainer,
+  Header,
+  LearnMore,
+  Title,
+  RelatedDiv,
+  RelatedMovies,
+  PageTurners,
+  PageTurnLeft,
+  PageTurnRight,
+  MoviesContainer
+} from './componentStyling/MainStyling.js';
 
 import Movies from './Movies.jsx';
 import Current from './Current.jsx';
-
-const MainContainer = styled.div`
-  width: 625px;
-  height: 265px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: right;
-`
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-`
-const LearnMore = styled.span`
-  font-size: 11px;
-  color: blue;
-  text-decoration: underline;
-  width: 16.33;
-  height: 13.33;
-  margin-right: 30px;
-  padding: 5px;
-  border-left: 1px dotted #CCC;
-  height: 15px;
-  cursor: pointer;
-`
-
-const Title = styled.span`
-  width: auto;
-  height: auto;
-  margin: 5px;
-  font-size: 24px;
-  font-family: Arial, sans-serif;
-  color: #424242fa;
-
-`
-const RelatedDiv = styled.div`
-  height: 290px;
-`
-
-const RelatedMovies = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 246px;
-  height: 238px;
-
-
-`
-const PageTurners = styled.div`
-  margin: 4px;
-  padding: 5px;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`
-
-const PageTurnLeft = styled.span`
-  cursor: pointer;
-  color: ${props => props.page === 'left' ? '#CCC' : 'blue'};
-  margin-right: 5px;
-`
-
-const PageTurnRight = styled.span`
-  cursor: pointer;
-  color: ${props => props.page === 'left' ? 'blue' : '#CCC'};
-  margin-left: 5px;
-`
-const MoviesContainer = styled.div`
-  overflow: hidden;
-`
 
 
 class Main extends React.Component{
